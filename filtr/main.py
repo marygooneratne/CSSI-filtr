250403dacef05bc2ee611a2733b3d3fa0ec47c0c
Demo User <demouser@MacBook-Air.local>
2017-07-27 10:29:43 -0700

    real deal

--- filtr/main.py
+++ filtr/main.py
@@ -8,7 +8,7 @@ from google.appengine.api import users
 from tweet_auths import OAuthAccessToken
 from tweet_auths import CONSUMER_KEY
 from tweet_auths import CONSUMER_SECRET
-
+from TwitterUserObject import TwitterUser
 ########################################################################
 # The following is necessary to make SSL work when running locally
 ########################################################################
@@ -97,7 +97,110 @@ class TweetsPage(webapp2.RequestHandler):
         template = env.get_template('templates/tweets_list.html')
         self.response.write(template.render(variables))
 
+class FollowersPage(webapp2.RequestHandler):
+
+    def get(self):
+        results_template = env.get_template('templates/followers.html')
+        current_user = users.get_current_user()
+        if not current_user:
+            self.redirect(users.create_login_url('/listfollowers'))
+            return
+
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        access_token = OAuthAccessToken.query(
+                OAuthAccessToken.user_id == current_user.user_id()
+                ).get()
+        if not access_token:
+            self.redirect('/authorize_twitter')
+            return
+        auth.set_access_token(access_token.token, access_token.token_secret )
+
+        api = tweepy.API(auth)
+        listoffriends = []
+        folks = api.followers()
+        for friend in folks:
+            listoffriends.append(friend.screen_name)
+        listoffriends.append('thehill')
+        listoffriends.append('CNN')
+        listoffriends.append('BW')
+        TwitterUser(name=api.me().screen_name, following=listoffriends, filteredfollowing=[]).put()
+
+        # We'll store the data in a dictionary, and use a template to render
+        # it nicely.
+        followersform = "<form action='/storeselected', method='get'>"
+        checker = TwitterUser.query(TwitterUser.name == api.me().screen_name).get()
+
+        for friend in checker.following:
+            followersform += ("<input type='checkbox' name='follower' value=%s>@%s<br>" % (friend, friend))
+
+        followersform += "<input type='submit' value='Submit'></form>"
+        template_variables = {'body':followersform, 'username':api.me().screen_name, 'profilepic': api.me().profile_image_url, 'description': api.me().description}
+        self.response.out.write(results_template.render(template_variables))
+
+class StoreSelected(webapp2.RequestHandler):
+    def get(self):
+        selected = self.request.get('follower', allow_multiple=True)
+        selectedfollowers = [];
+        for followername in selected:
+            selectedfollowers.append(followername)
+        current_user = users.get_current_user()
+        if not current_user:
+            self.redirect(users.create_login_url('/list_tweets'))
+            return
+
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        access_token = OAuthAccessToken.query(
+                OAuthAccessToken.user_id == current_user.user_id()
+                ).get()
+        if not access_token:
+            self.redirect('/authorize_twitter')
+            return
+        auth.set_access_token(access_token.token, access_token.token_secret )
+
+        api = tweepy.API(auth)
+        checkname = api.me().screen_name
+        changingUser = TwitterUser.query(TwitterUser.name == checkname).get()
+        changingUser.filteredfollowing = selectedfollowers
+        changingUser.put()
+        self.redirect('/filteredtimeline')
+
+class FilteredTimeline(webapp2.RequestHandler):
+    def get(self):
+        results_template = env.get_template('templates/timeline.html')
+        current_user = users.get_current_user()
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        access_token = OAuthAccessToken.query(OAuthAccessToken.user_id == current_user.user_id()).get()
+
+        if not access_token:
+            self.redirect('/authorize_twitter')
+            return
+
+        auth.set_access_token(access_token.token, access_token.token_secret)
+
+        api = tweepy.API(auth)
+
+        base_timeline = api.home_timeline()
+        new_timeline = []
+        checker = api.me().screen_name
+        selected = TwitterUser.query(TwitterUser.name == checker).get().filteredfollowing
+        for loopstatus in base_timeline:
+            if loopstatus.user.screen_name in selected:
+                new_timeline.append(loopstatus)
+
+        fillbody = "";
+
+        for tweet in new_timeline:
+            fillbody += ("<div class='tweet'> @" + tweet.author.screen_name+": " + tweet.text + "</div>")
+
+        template_variables = {'body':fillbody, 'username':api.me().screen_name, 'profilepic': api.me().profile_image_url, 'description': api.me().description}
+        self.response.out.write(results_template.render(template_variables))
+
+
+
 app = webapp2.WSGIApplication([
     ('/list_taytay_tweets', TweetsPage),
-    ('/', MainHandler),
+    ('/listfollowers', FollowersPage),
+    ('/filteredtimeline', FilteredTimeline),
+    ('/storeselected', StoreSelected),
+    ('/', MainHandler)
 ], debug=True)
